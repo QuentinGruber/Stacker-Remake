9f57b45319c7e9fa394595487c22a578688882f4
Kent1 <gruberquentin67@gmail.com>
2019-09-14 14:09:16 +0200

    added github icon

---
+++ Ui.js
@@ -0,0 +1,13 @@
+
+
+$("#github").click(function() {
+    document.location.href = "https://github.com/QGruber67/Stacker-Remake";
+});
+
+$( "#github" ).mouseover(function() {
+    $( "#github" ).css("opacity","0.5");
+  });
+
+ $( "#github" ).mouseout(function() {
+   $( "#github" ).css("opacity","1");
+});
