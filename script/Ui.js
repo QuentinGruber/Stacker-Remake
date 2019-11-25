353c0f84ab1b98b1eec72aaa99508f1bb4d238fa
QuentinGruber <QuentinGruber@gmail.com>
2019-11-25 15:32:05 +0100

    reformat code

--- script/Ui.js
+++ script/Ui.js
@@ -1,17 +1,17 @@
 
 
 // GITHUB
-$("#github").click(function() {
+$("#github").click(function () {
     document.location.href = "https://github.com/QGruber67/Stacker-Remake";
 });
 
-$( "#github" ).mouseover(function() {
-    $("#github").css("cursor","hand");
-    $( "#github" ).css("opacity","0.5");
-  });
+$("#github").mouseover(function () {
+    $("#github").css("cursor", "hand");
+    $("#github").css("opacity", "0.5");
+});
 
- $( "#github" ).mouseout(function() {
-   $( "#github" ).css("opacity","1");
+$("#github").mouseout(function () {
+    $("#github").css("opacity", "1");
 });
 
 // Mute
@@ -19,38 +19,38 @@ $( "#github" ).mouseover(function() {
 isMuted = false;
 hasClicked = false;
 
-$("#Mute").click(function() {
-    if (isMuted){
+$("#Mute").click(function () {
+    if (isMuted) {
         isMuted = false;
         hasClicked = true;
-        $( "#Mute" ).attr("src", "asset/mute_icon/on.png");
+        $("#Mute").attr("src", "asset/mute_icon/on.png");
     }
-    else{
+    else {
         isMuted = true;
         hasClicked = true;
-        $( "#Mute" ).attr("src", "asset/mute_icon/off.png");
+        $("#Mute").attr("src", "asset/mute_icon/off.png");
     }
 });
 
-$( "#Mute" ).mouseover(function() {
-    $("#Mute").css("cursor","hand");
-    if (isMuted){
-        $( "#Mute" ).attr("src", "asset/mute_icon/on.png");
+$("#Mute").mouseover(function () {
+    $("#Mute").css("cursor", "hand");
+    if (isMuted) {
+        $("#Mute").attr("src", "asset/mute_icon/on.png");
     }
-    else{
-        $( "#Mute" ).attr("src", "asset/mute_icon/off.png");
+    else {
+        $("#Mute").attr("src", "asset/mute_icon/off.png");
     }
-  });
+});
 
- $( "#Mute" ).mouseout(function() {
-    if (isMuted && !hasClicked){
-        $( "#Mute" ).attr("src", "asset/mute_icon/off.png");
+$("#Mute").mouseout(function () {
+    if (isMuted && !hasClicked) {
+        $("#Mute").attr("src", "asset/mute_icon/off.png");
     }
-    if(!isMuted && !hasClicked){
-        $( "#Mute" ).attr("src", "asset/mute_icon/on.png");
+    if (!isMuted && !hasClicked) {
+        $("#Mute").attr("src", "asset/mute_icon/on.png");
     }
-    if(hasClicked){
-        hasClicked=false;
+    if (hasClicked) {
+        hasClicked = false;
     }
 });
 
--- script/stacker.js
+++ script/stacker.js
@@ -20,33 +20,33 @@ Board = ArrayCreate(); //init our board
 
 
 
-function tableCreate(){
+function tableCreate() {
     var body = document.body,
-        tbl  = document.createElement('table');
+        tbl = document.createElement('table');
     tbl.id = "Board";
 
-    for(var i = 0; i < 15; i++){ //create row
+    for (var i = 0; i < 15; i++) { //create row
         var tr = tbl.insertRow();
-        for(var j = 0; j < 7; j++){//create column
+        for (var j = 0; j < 7; j++) {//create column
             var td = tr.insertCell();
             td.style.border = '1px solid black';
             td.style.backgroundColor = "white";
-            td.id = "c"+(j+1)+"r"+(i+1);
-            }
+            td.id = "c" + (j + 1) + "r" + (i + 1);
         }
+    }
 
     body.appendChild(tbl);
 }
 
 
-function ArrayCreate(){//create array for logic of the game
+function ArrayCreate() {//create array for logic of the game
     var row = [];
     var column = [];
 
     for (var i = 0; i < 15; i++) {
         column = [];
         for (var j = 0; j < 7; j++) {
-        column.push(0);
+            column.push(0);
         }
         row.push(column);
     }
@@ -54,16 +54,16 @@ function ArrayCreate(){//create array for logic of the game
 }
 
 
-function UpdateDisplay(){
+function UpdateDisplay() {
 
     for (var i = 0; i < 7; i++) {
-        if(Board[Ypos][i] ==1){
-            var Case = document.getElementById("c"+(i+1)+"r"+(Ypos+1));
-            Case.style.backgroundColor="blue";
+        if (Board[Ypos][i] == 1) {
+            var Case = document.getElementById("c" + (i + 1) + "r" + (Ypos + 1));
+            Case.style.backgroundColor = "blue";
         }
-        else{
-            var Case = document.getElementById("c"+(i+1)+"r"+(Ypos+1));
-            Case.style.backgroundColor="white";
+        else {
+            var Case = document.getElementById("c" + (i + 1) + "r" + (Ypos + 1));
+            Case.style.backgroundColor = "white";
 
         }
     }
@@ -72,63 +72,63 @@ function UpdateDisplay(){
 }
 
 
-function MoveBlue(x){
-    if(x==1){//if we go to right
-        Board[Ypos][Xpos-ExtraBlue]=0;
-        Xpos+=1;
-        Board[Ypos][Xpos]=1;
-        if(Xpos-ExtraBlue>=6){
-            GoingRight=0;
+function MoveBlue(x) {
+    if (x == 1) {//if we go to right
+        Board[Ypos][Xpos - ExtraBlue] = 0;
+        Xpos += 1;
+        Board[Ypos][Xpos] = 1;
+        if (Xpos - ExtraBlue >= 6) {
+            GoingRight = 0;
         }
 
     }
-    else{
-        Board[Ypos][Xpos+ExtraBlue]=0;
-        Xpos-=1;
-        Board[Ypos][Xpos]=1;
-        if(Xpos+ExtraBlue<=0){GoingRight=1;}
+    else {
+        Board[Ypos][Xpos + ExtraBlue] = 0;
+        Xpos -= 1;
+        Board[Ypos][Xpos] = 1;
+        if (Xpos + ExtraBlue <= 0) { GoingRight = 1; }
     }
 
 }
 
 
-function WhereToMove(){
-    if(GoingRight==1){
+function WhereToMove() {
+    if (GoingRight == 1) {
         MoveBlue(1);
     }
-    else{
+    else {
         MoveBlue(0);
     }
 }
 
 
-function CheckPlacement(){
+function CheckPlacement() {
     var SaveCase = []
     for (var i = 0; i < 7; i++) {
-        if(Board[Ypos][i] == 1 && Board[Ypos+1][i] != 1){
-            var Case = document.getElementById("c"+(i+1)+"r"+(Ypos+1));
-            Case.style.backgroundColor="white";
+        if (Board[Ypos][i] == 1 && Board[Ypos + 1][i] != 1) {
+            var Case = document.getElementById("c" + (i + 1) + "r" + (Ypos + 1));
+            Case.style.backgroundColor = "white";
             Board[Ypos][i] = 0; // change case value in board (needed form lose/win anim)
             SaveCase.push(Case)
             ExtraBlue -= 1
-            if(ExtraBlue<0){
+            if (ExtraBlue < 0) {
                 Loose();
-                setInterval(function(){LastRedBlinking(SaveCase);},500);
+                setInterval(function () { LastRedBlinking(SaveCase); }, 500);
             }
         }
     }
-    if(InGame && Score_enable){
-        Score+=1;
+    if (InGame && Score_enable) {
+        Score += 1;
 
-    var ScoreDisplay = document.getElementById("score");
-    ScoreDisplay.innerHTML="<h1>"+Score+"</h1>";
+        var ScoreDisplay = document.getElementById("score");
+        ScoreDisplay.innerHTML = "<h1>" + Score + "</h1>";
     }
 }
 
 
-function Loose(){
+function Loose() {
     InGame = 0;
-    AntiSpam=0;
+    AntiSpam = 0;
     MakeAllCube("red")
     //clear interval
     clearInterval(Interval_Wtm);
@@ -137,134 +137,134 @@ function Loose(){
 }
 
 
-function LastRedBlinking(SaveCase){
+function LastRedBlinking(SaveCase) {
     for (var i = 0; i < (SaveCase.length); i++) {
-        if(SaveCase[i].style.backgroundColor == "red" ){
-            SaveCase[i].style.backgroundColor="white";
+        if (SaveCase[i].style.backgroundColor == "red") {
+            SaveCase[i].style.backgroundColor = "white";
         }
-        else{
-            SaveCase[i].style.backgroundColor="red";
+        else {
+            SaveCase[i].style.backgroundColor = "red";
         }
     }
 }
 
 
-function Win(){
-    MakeAllCube("lime",Blinking = true)
+function Win() {
+    MakeAllCube("lime", Blinking = true)
     InGame = 0;
-    AntiSpam=0;
+    AntiSpam = 0;
 }
 
 
-function VictoryAnim(SaveCase){
+function VictoryAnim(SaveCase) {
     for (var i = 0; i < (SaveCase.length); i++) {
-        if(SaveCase[i].style.backgroundColor == "lime" ){
-            SaveCase[i].style.backgroundColor="white";
+        if (SaveCase[i].style.backgroundColor == "lime") {
+            SaveCase[i].style.backgroundColor = "white";
         }
-        else{
-            SaveCase[i].style.backgroundColor="lime";
+        else {
+            SaveCase[i].style.backgroundColor = "lime";
         }
     }
 }
 
 
-function MakeAllCube(color,Blinking = false){
+function MakeAllCube(color, Blinking = false) {
     var SaveCase = [];
     for (var i = 0; i < 15; i++) {
         for (var j = 0; j < 7; j++) {
             //Update display
-            if(Board[i][j] == 1){
-                var Case = document.getElementById("c"+(j+1)+"r"+(i+1));
-                if(Blinking){
+            if (Board[i][j] == 1) {
+                var Case = document.getElementById("c" + (j + 1) + "r" + (i + 1));
+                if (Blinking) {
                     SaveCase.push(Case)
                 }
-                else{
-                    Case.style.backgroundColor=color;
+                else {
+                    Case.style.backgroundColor = color;
                 }
             }
-            else{
-                var Case = document.getElementById("c"+(j+1)+"r"+(i+1));
-                Case.style.backgroundColor="white";
+            else {
+                var Case = document.getElementById("c" + (j + 1) + "r" + (i + 1));
+                Case.style.backgroundColor = "white";
 
             }
         }
     }
-    if (Blinking){
-    setInterval(function(){VictoryAnim(SaveCase);},200);
+    if (Blinking) {
+        setInterval(function () { VictoryAnim(SaveCase); }, 200);
     }
 }
 
 
-function TimeoutCountdown(){
+function TimeoutCountdown() {
     PlayTime -= 1;
     var plt = document.getElementById("Playtime");
-    plt.innerHTML = "<p id="+"timer"+">"+"Time left &nbsp&nbsp&nbsp"+PlayTime+"s"+"</p>";
-    if(PlayTime < 10){
-        $("#timer").css("color","red");
+    plt.innerHTML = "<p id=" + "timer" + ">" + "Time left &nbsp&nbsp&nbsp" + PlayTime + "s" + "</p>";
+    if (PlayTime < 10) {
+        $("#timer").css("color", "red");
     }
-    if(PlayTime == 0){
+    if (PlayTime == 0) {
         Loose();
     }
 
 }
 
 
-function Restart(){
+function Restart() {
     document.location.reload() // i know ...
 }
 
-function StopBlocks(KeyBoardAction = false){
-    if(!InGame){
+function StopBlocks(KeyBoardAction = false) {
+    if (!InGame) {
         Restart();
     }
-    if(AntiSpam == 0){
+    if (AntiSpam == 0) {
         //clear interval
         clearInterval(Interval_Wtm);
         clearInterval(Interval_Ud);
         clearInterval(Interval_Tc);
 
 
-        if(CanLose && InGame){CheckPlacement();} //you can't loose at first stage
+        if (CanLose && InGame) { CheckPlacement(); } //you can't loose at first stage
         CanLose = 1;//player can loose after first step
 
-        Ypos-=1;//up every movement
+        Ypos -= 1;//up every movement
 
 
         PlayTime = (21); //reset playtime
 
         TimeoutCountdown(); // display playtime updated
 
-        if(Ypos<0 && InGame){//Stop the game when we are at the top of the board
+        if (Ypos < 0 && InGame) {//Stop the game when we are at the top of the board
             Win();
         }
-        if(InGame){//to stop the game when you lost
+        if (InGame) {//to stop the game when you lost
 
 
-            if(Ypos == 11 && ExtraBlue == 2 ||Ypos == 7 && ExtraBlue == 1 ){
+            if (Ypos == 11 && ExtraBlue == 2 || Ypos == 7 && ExtraBlue == 1) {
                 ExtraBlue -= 1; //when progress in the game even without failure you will lost ExtraBlue
             }
 
-            Speed*=0.95; // 5% speed +
-            if (KeyBoardAction){AntiSpam=1;}//turn AntiSpam on after a keydown while still in game
+            Speed *= 0.95; // 5% speed +
+            if (KeyBoardAction) { AntiSpam = 1; }//turn AntiSpam on after a keydown while still in game
             LaunchBlue();
         }
 
     }
 }
 
-function LaunchBlue(){
-    Interval_Wtm = setInterval(function(){WhereToMove();}, Speed);
-    Interval_Ud = setInterval(function(){UpdateDisplay();}, 1);//display updated every 1msec
-    Interval_Tc = setInterval(function(){TimeoutCountdown();}, 1000); // -1 to PlayTime every 1 sec
+function LaunchBlue() {
+    Interval_Wtm = setInterval(function () { WhereToMove(); }, Speed);
+    Interval_Ud = setInterval(function () { UpdateDisplay(); }, 1);//display updated every 1msec
+    Interval_Tc = setInterval(function () { TimeoutCountdown(); }, 1000); // -1 to PlayTime every 1 sec
     // keyword
-    document.onkeydown = function(){
+    document.onkeydown = function () {
         StopBlocks(KeyBoardAction = true)
     }
-    document.onkeyup = function(){//turn off AntiSpam when the key is up
-        if(InGame){AntiSpam=0;} //only when in game
+    document.onkeyup = function () {//turn off AntiSpam when the key is up
+        if (InGame) { AntiSpam = 0; } //only when in game
     }
     // mouse/tap
-    document.onmousedown = function(){
+    document.onmousedown = function () {
         StopBlocks()
     }
 }
